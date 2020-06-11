12e7200704143bf143bd6e5759930da355edfec5
Dharmendra Sehgal <dharmendra.sehgal@gmail.com>
2020-06-11 13:23:35 +0530

    test commit for deployment verification

--- src/component.js
+++ src/component.js
@@ -1,4 +1,4 @@
-export default (text="Hello Webpack") => {
+export default (text="Hello Webpack with first deployment without gh pages") => {
     const elem = document.createElement("p");
     elem.innerHTML = text + '..!';
     return elem;
